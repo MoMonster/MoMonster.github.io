4b79530b63a0191c6aceb248909c8c981f56d9f3
Junwei Chen <JunweiC@gold-li.local>
2015-02-24 17:19:49 -0800

    Rotation
    .-.

--- gist9120416-f9066e9293cf8ba36d6e6b0b4cd6cc0c3aa1f251/clicks.js
+++ gist9120416-f9066e9293cf8ba36d6e6b0b4cd6cc0c3aa1f251/clicks.js
@@ -4,3 +4,15 @@ $("h1.page-title").text("My code runs!");
 $(window).on('click', function (e) {
     $('img.logo').css({left: e.pageX});
 });
+setInterval(function() {
+    $('.logo').css({
+        'margin-left': randomMargin(),
+        'margin-top': randomMargin()
+    });
+}, 200);
+var myColor = "blue"
+$(window).on('click', function (e) {
+    $('.logo').css({left: e.pageX,
+                    top: e.pageY});
+    $('.logo').toggleClass('rotated');
+});
